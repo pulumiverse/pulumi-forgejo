56dc77c7a7f41c1e94bcbcbcb5b1ea163fa7b81b
stoo johnston <stoo@pulumi.com>
2025-10-28 19:39:52 +0000

    start writing a fix for CreateTeam spec failure

--- provider/pkg/gen/openapi_fixes.go
+++ provider/pkg/gen/openapi_fixes.go
@@ -1,12 +1,35 @@
 package gen
 
 import (
+	"fmt"
+
 	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 )
 
 // FixOpenAPIDoc applies patches to the raw OpenAPI spec
 // before passing it to pulschema.
 func FixOpenAPIDoc(openAPIDoc *openapi3.T) error {
-	// TODO: Fill me in if there are patches to be made!
+	// fixCreateTeamOptionExample(openAPIDoc)
+	// return errors.New("Abandon ship")
 	return nil
+}
+
+// fixCreateTeamOptionExample fixes the example to be an object rather than a
+// string
+func fixCreateTeamOptionExample(openAPIDoc *openapi3.T) {
+	createTeamOptionProps, ok := openAPIDoc.Components.Schemas["CreateTeamOption"]
+	contract.Assertf(ok, "Expected to find CreateTeamOption type")
+	unitsMapProp, ok := createTeamOptionProps.Value.Properties["units_map"]
+	contract.Assertf(ok, "Expected to find 'units_map' property in CreateTeamOption type")
+	exampleProp := unitsMapProp.Value.Example
+	unitsMapProp.Value.Example = map[string]*openapi3.SchemaRef{
+		"first": openapi3.NewStringSchema().NewRef(),
+		"last":  openapi3.NewStringSchema().NewRef(),
+		"next":  openapi3.NewStringSchema().NewRef(),
+		"prev":  openapi3.NewStringSchema().NewRef(),
+	}
+	fmt.Printf("Example: %v\n", exampleProp)
+	fmt.Printf("Example fixed: %v\n", unitsMapProp.Value.Example)
+	fmt.Printf("Type: %v\n", unitsMapProp.Value.Type)
 }
